a6adaa51022e18bdde171bbcab6d38d3d2d8cd66
epaternina <epaternina9410@gmail.com>
2026-01-31 22:52:17 -0500

    Configuración de Firebase Hosting y README corregido

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+production: true,
+  firebase: {
+  apiKey: "AIzaSyB3s5ubcPhOSNa0VF3BLAq5Qtl39BK2698",
+  authDomain: "testionicedinson.firebaseapp.com",
+  projectId: "testionicedinson",
+  storageBucket: "testionicedinson.firebasestorage.app",
+  messagingSenderId: "145100685210",
+  appId: "1:145100685210:web:027c2f315e113f99b10423"
+  }
 };
